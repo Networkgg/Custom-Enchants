274aae65bc02acf9f75adc5e2305f9bebec66bad
GTNick <nickpp1111@yahoo.com>
2016-07-20 19:05:43 -0400

    Update MainClass.php

--- src/CE/MainClass.php
+++ src/CE/MainClass.php
@@ -2,3 +2,16 @@
 namespace CE;
 
 use pocketmine\Server
+use pocketmine\plugin\PluginBase
+
+class: 
+  
+      public function onEnable(){
+        $this->getLogger()->info("CE is now Enabled!");
+    }
+
+    public function onDisable(){
+        $this->getLogger()->info("CE is now Disabled!");
+    }
+}
+
